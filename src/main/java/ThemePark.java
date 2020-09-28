f1a0ee741f9dcc0d26b419138955f2a19cb05721
marekboro <marek.borowicz83@gmail.com>
2020-09-28 23:00:52 +0100

    updated ThemePark with methods to visit, getHashMap of ratings, getAllReviewed etc.

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,75 @@
+
+import attractions.Attraction;
+import behaviours.IReviewed;
+import behaviours.ISecurity;
+import people.Visitor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ThemePark {
+
+    private String parkName;
+    private ArrayList<IReviewed> reviewedItems;
+    private HashMap<String, Integer> ratings;
+
+    public ThemePark(String parkName) {
+        this.reviewedItems = new ArrayList<IReviewed>();
+        this.parkName = parkName;
+        this.ratings = new HashMap<String, Integer>();
+    }
+
+    public String getParkName() {
+        return parkName;
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        return reviewedItems;
+    }
+
+    public void visit(Visitor visitor, Attraction attraction) {
+        attraction.setVisitCount(attraction.getVisitCount() + 1);
+        visitor.addToAttractions(attraction);
+    }
+
+
+    public HashMap<String, Integer> getRatings() {
+        if (ratings.size() == 0){
+            updateRatingsFromReviewed();
+        }
+
+        return ratings;
+    }
+
+    public void addAttractionOrStall(IReviewed iReviewed) {
+        this.reviewedItems.add((IReviewed) iReviewed);
+    }
+
+    public void updateRatingsFromReviewed() {
+        for (IReviewed stallOrAttraction : reviewedItems) {
+            String name = stallOrAttraction.getName();
+            int rating = stallOrAttraction.getRating();
+
+            ratings.put(name, rating);
+        }
+
+    }
+
+    public ArrayList<IReviewed> getAllAllowedFor(Visitor visitor) {
+        ArrayList<IReviewed> toBeReturned = new ArrayList<IReviewed>();
+
+        for (IReviewed stallOrAttraction : reviewedItems) {
+            if (stallOrAttraction instanceof ISecurity) {
+                if(((ISecurity) stallOrAttraction).isAllowed(visitor)){
+                    toBeReturned.add(stallOrAttraction);
+                }
+
+            } else {
+                toBeReturned.add(stallOrAttraction);
+            }
+        }
+        return toBeReturned;
+    }
+
+
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,85 @@
+import attractions.*;
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+
+    ThemePark themePark;
+    Dodgems dodgems;
+    Park park;
+    Playground playground;
+    RollerCoaster rollerCoaster;
+    CandyflossStall candyStall;
+    IceCreamStall iceCreamStall;
+    TobaccoStall tobaccoStall;
+    Visitor visitor1;
+    Visitor visitor2;
+    Visitor visitor3;
+    Visitor visitor4;
+
+    @Before
+    public void setUp() throws Exception {
+
+        visitor1 = new Visitor(18, 180.0, 30.0);
+        visitor2 = new Visitor(17, 120.0, 20.0);
+        visitor3 = new Visitor(15, 120.0, 20.0);
+        visitor4 = new Visitor(15, 150.0, 20.0);
+
+        themePark = new ThemePark("Camelot");
+        dodgems = new Dodgems("The Italian Job", 9);
+        park = new Park("Burial grounds", 6);
+        playground = new Playground("Quicksands",5);
+        rollerCoaster = new RollerCoaster("Purple dragon",10);
+        candyStall = new CandyflossStall("Dentist's joy","Dr Nick", ParkingSpot.A1);
+        iceCreamStall = new IceCreamStall("Ice'n'Berg's","Walter",ParkingSpot.A3);
+        tobaccoStall = new TobaccoStall("Steam Powered","Cid Garr",ParkingSpot.B2);
+
+        themePark.addAttractionOrStall(dodgems);
+        themePark.addAttractionOrStall(park);
+        themePark.addAttractionOrStall(playground);
+        themePark.addAttractionOrStall(rollerCoaster);
+        themePark.addAttractionOrStall(candyStall);
+        themePark.addAttractionOrStall(iceCreamStall);
+        themePark.addAttractionOrStall(tobaccoStall);
+        themePark.updateRatingsFromReviewed();
+
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Camelot",themePark.getParkName());
+    }
+
+    @Test
+    public void hasReviewedItems(){
+        assertEquals(7,themePark.getAllReviewed().size());
+    }
+
+    @Test
+    public void hasRatings(){
+        int a = themePark.getRatings().get("The Italian Job");
+        assertEquals(9,a);
+        int size = themePark.getRatings().size();
+        assertEquals(7,size);
+
+    }
+
+    @Test
+    public void allowedForVisitorSizeChangesDependingOnVisitorParameter(){
+        assertEquals(6,themePark.getAllAllowedFor(visitor1).size());
+        assertEquals(4,themePark.getAllAllowedFor(visitor2).size());
+        assertEquals(5,themePark.getAllAllowedFor(visitor3).size());
+        assertEquals(6,themePark.getAllAllowedFor(visitor4).size());
+    }
+
+
+
+
+}
